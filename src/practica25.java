5d517b9db57b4d4f6ef9d9bc90331bf2af6062e3
LizMarian <lizmarianbonilla@gmail.com>
2025-10-03 14:37:40 -0400

    first commit

---
+++ src/practica25.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class practica25{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        {
+        System.out. println("diganme su edad ");
+    integer edad= sc.nextint();
+edad>18? "Eres mayor de edad " : "Eres menor de edad  "}
+System.out.Printin(msg);
+}
+        }
